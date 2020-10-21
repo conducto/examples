5b4ed9358c2f07e43cba4e437ca046d29f81994b
M@ Rixman <github@matt.rixman.org>
2020-10-21 16:46:58 -0600

    updated git example

--- hello_git/README.md
+++ hello_git/README.md
@@ -1,18 +1,29 @@
 # hello_py
 
-A repo-sourced hello world, with and without a local path mapping.
+A repo-sourced hello world.  Compares explicit references to an external repo with implicit references to its own repo.
 
-### To Run
+### External Repo, No Local Code
 
-Without `path_map`
+Use `copy_url` to clone code from an external repo:
 
     python ./pipeline.py --local
 
-With `path_map`
+### External Repo, Local Code for Debug Sessions
+
+Like above, but use `path_map` to mount local code in `./local-copy` for use in debug sessions:
 
     python ./pathmap.py --local
 
 Note how contents of [./local-copy](./local_copy) resemble the contents of https://github.com/leachim6/hello-world.
+In live debug sessions, they'll be mounted instead of the files from the repo so that you can try uncommitted changes in the container.
+
+### This Repo, Local Changes Ignored
+
+
+Use `copy_repo` to detect the parent repo (this one) and clone it:
+
+    python ./pathmap.py --local copy
+
 
 ### Related
 
--- hello_git/pipeline.py
+++ hello_git/pipeline.py
@@ -1,17 +1,40 @@
 import conducto as co
 
-python_img = co.Image(
+# Include an external git repo in the image
+############################################
+
+external_img = co.Image(
     image="python:3.8-alpine",
     copy_url="https://github.com/leachim6/hello-world",
     copy_branch="master",
 )
 
-
-def hello() -> co.Serial:
-    pipeline = co.Serial()
-    pipeline["Say Hi"] = co.Exec("python p/python3.py", image=python_img)
+# reference code in that repo
+def hello_other() -> co.Serial:
+    pipeline = co.Serial(image=external_img)
+    pipeline["Say Hi"] = co.Exec("python p/python3.py")
     return pipeline
 
+# Include this git repo in the image
+#####################################
+
+examples_img = co.Image(image="python:3.8-alpine", copy_repo=True, reqs_py=['conducto'])
+
+# plucks the hello-world pipeline out of it
+def get_pipeline() -> co.Serial:
+    from hello_world import hello
+    return hello.pipeline()
+
+
+# reference code in this repo
+def hello_self() -> co.Serial:
+    pipeline = co.Serial(image=examples_img, env={"PYTHONPATH":"."})
+    pipeline["Say Hi"] = co.Lazy(get_pipeline)
+    return pipeline
+
+# co.Lazy builds the rest of the pipeline tree at runtime, so if new nodes are
+# added to ../hello_world.pipeline.py in the future, those nodes will show up
+# without needing to change this file
 
 if __name__ == "__main__":
-    co.main(default=hello)
+    co.main(default=hello_other)
