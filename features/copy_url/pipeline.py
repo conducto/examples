a0971ababc96b202156820a4b6ddae24b9f0f40d
Jonathan Marcus <jmarcus@conducto.com>
2020-11-17 18:16:53 -0500

    rename data_science/toy -> features/copy_url

---
+++ features/copy_url/pipeline.py
@@ -0,0 +1,20 @@
+import conducto as co
+
+# Build a Docker image using contents of a Git repo
+IMG = co.Image("python:3.8-slim",
+    copy_url="https://github.com/conducto/examples", copy_branch="main",
+    reqs_packages=["cloc"], reqs_py=["pandas"]
+)
+
+def main() -> co.Parallel:
+    with co.Parallel(image=IMG) as root:
+        # Count lines of code in the remote Git repo.
+        root["lines of code"] = co.Exec("cloc .")
+        # Run a simple data analysis script located there.
+        root["biggest US cities"] = co.Exec(
+            "cd features/copy_url && python analyze.py cities.csv"
+        )
+    return root
+
+if __name__ == "__main__":
+    co.main(default=main)
