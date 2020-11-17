cba3172f2b6da05da6a46bbfd9d9399ecbaa13cd
Matt Rixman <mrixman@conducto.com>
2020-11-16 23:56:05 -0700

    Simplified two-microservice example
    And made it look like the other CI/CD examples

--- cicd/local_microservices/README.md
+++ cicd/local_microservices/README.md
@@ -1,4 +1,4 @@
-# counter
+# Local Microservices
 
 This example deploys, tests, and cleans up a simple app.
 The app is made of two services: [Redis](https://redis.io) and [Flask](https://flask.palletsprojects.com).
@@ -6,19 +6,8 @@ The app is made of two services: [Redis](https://redis.io) and [Flask](https://f
 - Redis stores the counter.
 
 The pipeline deploys them as docker containers, and tests them.
-There is a clean-up step too, but it starts skipped so that you can play around manually before tearing it down.
+There is a clean-up step which stops the containers at the end.
 
 ### To Run
 
     python ./pipeline.py --local
-
-### While it's up
-
-- Take your browser to [http://localhost:5000](http://localhost:500) and hit refesh a few times.
-- Run `docker ps` to see the services that we deployed.
-- Run `docker logs <contaier-id>` to see what's up with the deployed services.
-
-### Tear it down
-
-- When you're done, unskip the last node and let it clean up.
-- Run `docker ps` again to see that the services are gone.
--- cicd/local_microservices/pipeline.py
+++ cicd/local_microservices/pipeline.py
@@ -1,183 +1,79 @@
+"""
+### Two Local Microservices
+
+This pipeline tests connectivity between two microservices.
+
+The test involves sending an http request to flask, which increments a
+counter in redis.  Flask responds with the new value of that counter.
+
+It deploys to the local docker instance, which is atypical for CI/CD,
+but makes for a self-contained example.
+"""
+
 import conducto as co
-import json
-import os
-
-# we need two custom images
-# other images are used unaltered from dockerhub
-docker_img = co.Image(image="docker:latest", copy_dir=".", reqs_py=["conducto"])
-test_img = co.Image(
-    image="python:latest", copy_dir=".", reqs_py=["conducto", "redis", "requests"]
-)
 
 
-# Test Functions
-################
-
-# The functions below are called directly by exec nodes in the
-# Pipeline Definitions section
-
-
-def redis_up():
-    """
-    Is Redis Up?
-    """
-
-    # not a dependency of user-side code
-    # but required once inside the pipeline
-    # (provided by 'reqs_py` kwarg to test_img)
-    import redis
-
-    ip = co.data.pipeline.gets("/redis/ip").decode()
-    r = redis.Redis(host=ip, port=6379, db=0)
-    r.ping()
-
-
-def flask_up():
-    """
-    Is Flask Up?
-    """
-
-    import requests
-
-    ip = co.data.pipeline.gets("/flask/ip").decode()
-    url = f"http://{ip}:5000"
-    resp = requests.get(url)
-    if resp.status_code >= 300:
-        print(resp.text)
-        raise Exception(f"flask service responded {resp.status_code}")
-
-
-def integration_test():
-    """
-    Do subsequent calls to flask cause an incremented response?
-    (a counter is stored in Redis)
-    """
-
-    import requests
-    import re
-
-    # prep
-    ip = co.data.pipeline.gets("/flask/ip").decode()
-    url = f"http://{ip}:5000"
-    print(url)
-
-    def get_num(resp):
-        text = resp.text.strip()
-        print("got:", text)
-        num = re.search(f"[0-9]+", text).group(0)
-        return int(num)
-
-    # two requests
-    one = get_num(requests.get(url))
-    two = get_num(requests.get(url))
-
-    # did the response increment?
-    if (one + 1) == two:
-        print("increment detected")
-    else:
-        raise Exception(f"got {one} then {two}")
-
-
-# Pipeline Definitions
-######################
-
-# Type hinted functions that return Conducto nodes can be called from the CLI.
-# This will create pipeline instances with contents restricted to that function.
-# Or call one pipeline-defining function from another to create larger pipelines
-# though composition.  Pipeline nodes are python objects.
-
-# To launch a local pipeline from this definition:
-# $ python pipeline.py --local
-
-
-def pipeline() -> co.Serial:
+def main() -> co.Serial:
     """
     Starts services, tests them, and cleans up
     """
 
-    # don't stop on errors so that clean up still runs if something goes wrong
-    with co.Serial(stop_on_error=False) as root:
+    # outer context: continue on errors
+    # so 'clean up' still runs if tests fail
+    with co.Serial(stop_on_error=False, doc=__doc__) as root:
 
-        with co.Serial(name="run", stop_on_error=True) as test:
-
-            # start services, sanity-check that they're indeed up
-            test["deploy"] = deploy()
-            test["test_up"] = test_up()
-
-            # test that they work together
-            test["integration test"] = co.Exec(integration_test)
-            test["integration test"].image = test_img
+        # inner context: stop on errors
+        # don't bother testing a failed deployment
+        with co.Serial(name="run", stop_on_error=True) as run:
+            run["deploy"] = deploy()
+            run["test"] = test()
 
         # stop services
         root["clean up"] = teardown()
 
-        # start with cleanup skipped so that app stays deployed
-        # manually unskip this node to trigger cleanup
-        root["clean up"].skip = True
-
     return root
 
 
-# To launch a local pipeline from this definition:
-# $ python pipeline.py deploy --local
+# test tools
+test_img = co.Image(reqs_packages=["redis", "curl"], reqs_py=["conducto"])
+
+
+def test() -> co.Serial:
+    """
+    Check if both redis and flask are available.  Then see if they're
+    working.
+    """
+
+    with co.Serial(image=test_img) as test:
+
+        with co.Parallel(name="services up?") as check:
+            check["redis up?"] = co.Exec(TEST_REDIS_CMD)
+            check["flask up?"] = co.Exec(TEST_FLASK_CMD)
+
+    test["integration test"] = co.Exec(INTEGRATION_TEST_CMD)
+    return test
+
+
+# deployment tools
+docker_img = co.Image("docker:latest", reqs_py=["conducto"], copy_dir=".")
 
 
 def deploy() -> co.Parallel:
     """
-    Starts services and extracts runtime details
+    Start Containers.
     """
-    with co.Serial(image=docker_img, requires_docker=True) as node:
+
+    with co.Parallel(image=docker_img, requires_docker=True) as node:
 
         # use the redis image from dockerhub
         with co.Serial(name="redis") as redis:
-            redis["start"] = co.Exec(
-                """
-                set -euo pipefail
-                docker inspect \\
-                  $(docker run --rm -d \\
-                      --network conducto_network_$CONDUCTO_PIPELINE_ID \\
-                      redis:alpine) \\
-                  | tee \\
-                  | conducto-data-pipeline puts /redis/inspect
-                """
-            )
-            redis["container metadata"] = co.Exec(extract, "redis")
+            redis["start"] = co.Exec(START_REDIS_CMD)
 
-        # incloud our flask code via a Dockerfile
+        # include our flask code via a Dockerfile
         with co.Serial(name="flask") as flask:
-            flask["build"] = co.Exec("docker build . -t myflask:latest")
-            flask["start"] = co.Exec(
-                """
-                set -euo pipefail
-                docker inspect \\
-                  $(docker run --rm -d \\
-                      --network conducto_network_$CONDUCTO_PIPELINE_ID \\
-                      -p 5000:5000 \\
-                      -e REDIS_IP=$(conducto-data-pipeline gets /redis/ip) \\
-                      myflask:latest) \\
-                  | tee \\
-                  | conducto-data-pipeline puts /flask/inspect
-                """
-            )
-            flask["container metadata"] = co.Exec(extract, "flask")
+            flask["build"] = co.Exec(BUILD_FLASK_CMD)
+            flask["start"] = co.Exec(START_FLASK_CMD)
 
-    return node
-
-
-# In this example, container id's are stored on the launched pipeline instance.
-# Unlike the pipeline-node-returning functions above, the ones below can't be
-# called independently from the CLI because the new pipeline wouldn't have the
-# data that they need) you could work around this by using `conducto-data-user`
-# instead.
-
-
-def test_up() -> co.Parallel:
-    """
-    Docker returned, but are the services really up?
-    """
-    with co.Parallel(image=test_img) as node:
-        node["ping redis"] = co.Exec(redis_up)
-        node["hit flask"] = co.Exec(flask_up)
     return node
 
 
@@ -186,57 +82,83 @@ def teardown():
     Stop containers.
     """
     with co.Parallel(image=docker_img, requires_docker=True) as node:
-        node["redis"] = co.Exec("docker stop $(conducto-data-pipeline gets /redis/id)")
-        node["flask"] = co.Exec("docker stop $(conducto-data-pipeline gets /flask/id)")
+        node["stop redis"] = co.Exec(STOP_REDIS_CMD)
+        node["stop flask"] = co.Exec(STOP_FLASK_CMD)
     return node
 
 
-# Helper Functions
-##################
+########################################################################
+# Commands
+########################################################################
 
+START_REDIS_CMD = """
+set -euxo pipefail
 
-def extract(path):
-    """
-    path: Given a service name like "foo" where pipeline://foo/inspect contains
-    the output of docker inspect for that service's container.
+# Kill previous container, if any
+docker container rm my_redis 2>/dev/null || true
 
-    effect:
-      - store the container's conducto-facing IP address in /foo/ip.
-      - store the container's ID in /foo/id
-    """
+# Run container on pipeline network
+docker run --rm -d \\
+       --name my_redis \\
+       --network conducto_network_${CONDUCTO_PIPELINE_ID} \\
+       redis:alpine
 
-    inspect_key = f"/{path}/inspect"
-    ip_key = f"/{path}/ip"
-    id_key = f"/{path}/id"
+# Stash its IP for testing
+docker inspect my_redis \\
+    --format '{{index .NetworkSettings.Networks "conducto_network_'$CONDUCTO_PIPELINE_ID'" "IPAddress" }}' \\
+    | conducto-data-pipeline puts /redis/ip
+"""
 
-    # get prerequisite data
-    pipeline_id = os.environ["CONDUCTO_PIPELINE_ID"]
-    print("key:", inspect_key)
-    # sleep(1) # lose the race with node that saves it
-    print("val:", co.data.pipeline.gets(inspect_key).decode())
-    inspection = json.loads(co.data.pipeline.gets(inspect_key).decode())[0]
+TEST_REDIS_CMD = """
+set -ex
+REDIS_IP=$(conducto-data-pipeline gets /redis/ip)
+redis-cli -h $REDIS_IP -p 6379 ping | grep PONG
+"""
 
-    # stash container id too
-    co.data.pipeline.puts(id_key, inspection["Id"].encode())
+BUILD_FLASK_CMD = "docker build . -t myflask:latest"
 
-    # get this container's IP address
-    network_names = []
-    address = None
-    for name, network in inspection["NetworkSettings"]["Networks"].items():
-        network_names.append(name)
-        if pipeline_id in name:
-            address = network["IPAddress"]
-            print(address)
-            co.data.pipeline.puts(ip_key, address.encode())
+START_FLASK_CMD = """
+set -euxo pipefail
 
-    if not address:
-        raise Exception(
-            f"CONDUCTO_PIPELINE_ID = {pipeline_id}, "
-            f"but no matching network was found among {json.dumps(network_names)}"
-        )
+# Kill previous container, if any
+docker container rm my_flask 2>/dev/null || true
 
+# Run container on pipeline network
+docker run --rm -d \\
+      --name my_flask \\
+      --network conducto_network_${CONDUCTO_PIPELINE_ID} \\
+      -p 5000:5000 \\
+      -e REDIS_IP=$(conducto-data-pipeline gets /redis/ip) \\
+      myflask:latest
+
+# Stash its IP for testing
+docker inspect my_flask \\
+    --format '{{index .NetworkSettings.Networks "conducto_network_'$CONDUCTO_PIPELINE_ID'" "IPAddress" }}' \\
+    | conducto-data-pipeline puts /flask/ip
+"""
+
+TEST_FLASK_CMD = """
+set -ex
+FLASK_IP=$(conducto-data-pipeline gets /flask/ip)
+curl $FLASK_IP:5000
+"""
+
+INTEGRATION_TEST_CMD = """
+set -ex
+
+# two requests for flask
+FLASK_IP=$(conducto-data-pipeline gets /flask/ip)
+curl -s $FLASK_IP:5000 | egrep -o '[0-9]+' > first
+curl -s $FLASK_IP:5000 | egrep -o '[0-9]+' > second
+
+# did they increment?
+let FIRST_PLUS_ONE="$(cat first)+1"
+cat second | grep $FIRST_PLUS_ONE
+"""
+
+STOP_REDIS_CMD = "docker stop my_redis"
+
+STOP_FLASK_CMD = "docker stop my_flask"
 
 if __name__ == "__main__":
-
-    # this sets "pipeline" as the default pipeline-defining function
-    co.main(default=pipeline)
+    co.main(default=main)
