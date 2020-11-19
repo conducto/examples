303079edc50fe67d7bd34366f62e0a023713d28a
Matt Rixman <mrixman@conducto.com>
2020-11-19 10:27:52 -0700

    a simplifying refactor

--- cicd/remote_microservices/pipeline.py
+++ cicd/remote_microservices/pipeline.py
@@ -14,161 +14,82 @@ You'll need some external account to make it work.  To configure access
 to the necessary infrastructure services, see access.py.
 """
 
-from collections import namedtuple
 import conducto as co
 
-# see access.py for auth stuff
+# see access.py to set credential
 import access
 
+# we'll need these tools
+infractl_image = "finalgene/heroku-cli"
+test_image = co.Image(reqs_packages=["redis", "curl"])
 
-def main(touch_prod: bool, code_source: str) -> co.Serial:
+
+def main(src_image, test_env, prod_env=None) -> co.Serial:
     """
     Test the code, deploy if appropriate.
     """
 
-    # determine where the code should come from and where it's going
-    params = plan(touch_prod, code_source)
-
-    with co.Serial(doc=__doc__) as root:
+    with co.Serial(doc=__doc__, image=src_image, env=test_env) as root:
 
         # can we autenticate?  If not, fail early.
-        root["access check"] = access_check(params)
+        with co.Parallel(name="access check"):
 
-        with co.Serial(name="test", env=params.test_env) as test:
+            co.Exec(TEST_HEROKU_CMD, image=infractl_image, name="Heroku")
+            co.Exec(TEST_REDIS_CMD, image=test_image, name="RedisLabs")
 
-            test["deploy"] = deploy(params, test_env=True)
-            test["test"] = co.Exec(INTEGRATION_TEST_CMD)
+        # test the code in a test env
+        with co.Serial(name="test"):
 
-        # will be skipped if prod deploy isn't called for
-        root["deploy"] = deploy(params)
+            deploy(src_image, test_env, name="deploy")
+            co.Exec(INTEGRATION_TEST_CMD, name="test")
 
-        # will start skipped.  Manually unskip to undeploy.
-        # root["teardown"] = teardown_all(params)
+        # true for CD only
+        if prod_env:
+            deploy(src_image, prod_env, name="deploy")
 
-        # uncommenting "teardown" causes "access check" to have no env vars
-        # why?
+        # # Unskip a child to undeploy
+        # with co.Serial(skip=True, name="teardown"):
+        #     teardown(test_env, name=test_env["NAME"])
+        #     if prod_env:
+        #         teardown(prod_env, name=prod_env["NAME"])
+
+        # why does enabling "/teardown" break "/acces check"?
+        # it somehow clobbers the env vars
 
     return root
 
 
-# These parameters determine:
-#  - local code or repo code?
-#  - test only or test + deploy
-Plan = namedtuple("Plan", "src_image test_image src_path test_env target_env")
-
-
-def plan(touch_prod: bool, code_source: str) -> Plan:
-    """
-    Initialize parameters depending on how the pipeline is to be launched
-    """
-
-    def get_src_img(**kwargs):
-        return co.Image(image="finalgene/heroku-cli", reqs_packages=["git"], **kwargs)
-
-    test_img = co.Image(reqs_packages=["redis", "curl"])
-    test_env = access.env("test")
-    test_env_name = test_env["NAME"]
-
-    repo = "https://github.com/conducto/examples"
-    branch = code_source
-
-    # where should the code come from?
-    if code_source.lower() == "filesystem":
-
-        print(f"Testing code from local filesystem in {test_env_name}")
-        src_img = get_src_img(copy_dir=".")
-        src_path = "."
-
-    elif code_source.lower() == "pr":
-
-        print(f"Testing branch indicated by PR in {test_env_name}")
-        src_img = get_src_img(copy_repo=True)
-        src_path = "./cicd/remote_microservices"
-
-    else:
-
-        print(f"Testing code from {repo}:{branch} in {test_env_name}")
-        src_img = get_src_img(copy_url=repo, copy_branch=branch)
-        src_path = "./cicd/remote_microservices"
-
-    if touch_prod:
-        # CI + CD
-        target_env = access.env("prod")
-        target_name = target_env["NAME"]
-        print(f"If it passes, will deploy to {target_name}")
-
-    else:
-        # Just CI
-        target_env = test_env
-
-    return Plan(src_img, test_img, src_path, test_env, target_env)
-
-
-def access_check(params: Plan) -> co.Parallel:
-    """
-    Test available credentials against infra providers.
-    """
-
-    with co.Parallel(env=params.test_env) as node:
-        node["Heroku"] = co.Exec(TEST_HEROKU_CMD, image=params.src_image)
-
-        node["RedisLabs"] = co.Exec(TEST_REDIS_CMD, image=params.test_image)
-
-    return node
-
-
-def deploy(params: Plan, test_env=False) -> co.Serial:
+def deploy(src_image: co.Image, env: dict, **kwargs) -> co.Serial:
     """
     Put the code in an environment.
-    Which code is determed by param.src_image
-    Which environment is determed by target_env
     """
 
-    # pick correct deploy target
-    if test_env:
-        env = params.test_env
-    else:
-        env = params.target_env
+    with co.Serial(env=env, **kwargs) as node:
 
-        # did a previous node already deploy here?
-        if env == params.test_env:
-
-            # do nothing
-            doc = "This is not a CD pipeleine"
-            with co.Serial(doc=doc) as node:
-                co.Exec(":", name=access.uniqstr("prod"), skip=True, doc=doc)
-            return node
-
-    # onward to production
-    with co.Serial(image=params.src_image) as node:
+        # deploy the code
         with co.Serial(name=env["NAME"]) as deploy:
-            deploy["create"] = co.Exec(CREATE_APP_CMD)
-            deploy["push"] = co.Exec(PUSH_CODE_CMD.format(params.src_path))
 
-        # was the deploy successful?
-        node["sanity check"] = co.Exec(TEST_FLASK_CMD)
+            deploy["create"] = co.Exec(CREATE_APP_CMD)
+
+            push_cmd = PUSH_CODE_CMD.format(src_image.path_to_code)
+            deploy["push"] = co.Exec(push_cmd)
+
+        # fail if we can't see the service
+        node["sanity check"] = co.Exec(TEST_FLASK_CMD, image=test_image)
 
     return node
 
 
-def teardown_all(params: Plan):
+def teardown(env: dict, **kwargs) -> co.Parallel:
     """
-    Unskip a child to un-deploy an environment.
+    Un-deploy an environment.
     """
 
-    def teardown_one(env):
-        with co.Parallel(name=env["NAME"], env=env) as down:
-            down["clear data"] = co.Exec(CLEAR_DATA_CMD, image=params.test_image)
-            down["stop flask"] = co.Exec(STOP_FLASK_CMD, image=params.src_image)
+    with co.Parallel(env=env, **kwargs) as down:
+        down["clear data"] = co.Exec(CLEAR_DATA_CMD, image=test_image)
+        down["stop flask"] = co.Exec(STOP_FLASK_CMD, image=infractl_image)
 
-    with co.Parallel(skip=True) as teardown:
-
-        # only offer to teardown both environments if they're distinct
-        teardown_one(params.test_env)
-        if params.test_env != params.target_env:
-            teardown_one(params.target_env)
-
-    return teardown
+    return down
 
 
 ########################################################################
@@ -245,47 +166,74 @@ STOP_FLASK_CMD = "docker stop my_flask"
 # Entrypoints
 ########################################################################
 
-
 # .conducto.cfg determines which of these functions gets called in
 # response to source control activity
 
 # if conducto is installed locally, you can also call them with a command:
 
-#   python pipeline.py test_uncommitted --local --run
-def test() -> co.Serial:
-    """
-    Test changes before pushing them
-    """
-    return main(touch_prod=False, code_source="filesystem")
+test_env = access.env("test")
+test_env_name = test_env["NAME"]
 
 
-#   python pipeline.py pr from_branch --cloud --run
+def testlocal() -> co.Serial:
+    """
+    Triggered by a human
+    """
+
+    src_img = get_src_img(copy_dir=".")
+    src_img.path_to_code = "."
+
+    print(f"Testing code from local filesystem in {test_env_name}")
+
+    return main(src_img, test_env)
+
+
 def pr(branch: str) -> co.Serial:
     """
-    Triggered from a PR
-    or when testing CI by hand
+    Triggered by a PR
     """
-    return main(touch_prod=False, code_source="pr")
+
+    src_img = get_src_img(copy_repo=True)
+    src_img.path_to_code = "./cicd/remote_microservices"
+
+    print(f"Testing branch indicated by PR in {test_env_name}")
+
+    return main(src_img, test_env)
 
 
-#   python pipeline.py merge --cloud --run
 def merge() -> co.Serial:
     """
-    Triggered from a merge
-    or when testing CD by hand
+    Triggered from a merge to "main"
     """
 
-    return main(touch_prod=True, code_source="main")
+    prod_branch = "main"
+    repo = "https://github.com/conducto/examples"
+
+    src_img = get_src_img(copy_url=repo, copy_branch=prod_branch)
+    src_img.path_to_code = "./cicd/remote_microservices"
+
+    print(f"Testing branch: {prod_branch} in  {test_env_name}")
+
+    prod_env = access.env("prod")
+    prod_env_name = prod_env["Name"]
+
+    print(f"...if it passes, will deploy to {prod_env_name}")
+
+    return main(src_img, test_env, prod_env=prod_env)
 
 
-#   python pipeline.py
+# Helper for injecting code into the deploy image
+def get_src_img(**kwargs):
+    return co.Image(image=infractl_image, reqs_packages=["git"], **kwargs)
+
+
 if __name__ == "__main__":
 
     # If you're in a sandbox, uncomment only one of these or install
     # condcuto for local control
 
     # test local code
-    co.main(default=test)
+    co.main(default=testlocal)
 
     # test code from git branch
     # co.main(argv=["pr", "my_feature"])
