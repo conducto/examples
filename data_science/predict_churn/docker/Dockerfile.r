5a9ee2b5e640da0fa8febdff928f1b051b412588
Matt Jachowski <mjachowski@conducto.com>
2020-11-24 01:25:29 +0000

    make predict churn example easier to follow

--- data_science/predict_churn/docker/Dockerfile.r
+++
@@ -1,8 +0,0 @@
-FROM python:3.8
-
-# Install R.
-RUN apt-get update && apt-get install -y r-base
-RUN Rscript -e 'install.packages(c("jsonlite", "argparser"))'
-
-# Install python packages.
-RUN pip3 install conducto
